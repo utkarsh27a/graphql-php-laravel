8b77237bf62f0f1e30738f71204d256ec7abcc76
Utkarsh Pandey <utkarsh.pandey@4thpointer.com>
2020-07-29 06:27:00 +0530

    createed wine store app

---
+++ app/GraphQL/Queries/WineQuery.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Models\Wine;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Query;
+
+class WineQuery extends Query
+{
+    protected $attributes = [
+        'name' => 'wine',
+    ];
+
+    public function type(): Type
+    {
+        return GraphQL::type('Wine');
+    }
+
+    public function args():array
+    {
+        return [
+            'id' => [
+                'name' => 'id',
+                'type' => Type::int(),
+                'rules' => ['required']
+            ],
+        ];
+    }
+
+    public function resolve($root, $args)
+    {
+        return Wine::findOrFail($args['id']);
+    }
+}
---
+++ app/GraphQL/Queries/WinesQuery.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\GraphQL\Queries;
+
+use App\Models\Wine;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Query;
+
+class WinesQuery extends Query
+{
+    protected $attributes = [
+        'name' => 'wines',
+    ];
+
+    public function type(): Type
+    {
+        return Type::listOf(GraphQL::type('Wine'));
+    }
+
+    public function resolve($root, $args)
+    {
+        return Wine::all();
+    }
+}
